2e23b7be037eb55c66bc801a171e69862a601820
Connor Turland <connorturland@gmail.com>
2018-09-10 16:52:24 -0400

    proper namespace for imports

--- index.ts
+++ index.ts
@@ -16,25 +16,27 @@ export {
   free
 }
 
-declare function hc_debug(encoded_allocation_of_input: u32): u32;
-declare function hc_call(encoded_allocation_of_input: u32): u32;
-declare function hc_sign(encoded_allocation_of_input: u32): u32;
-declare function hc_verify_signature(encoded_allocation_of_input: u32): u32;
-declare function hc_commit_entry(encoded_allocation_of_input: u32): u32;
-declare function hc_update_entry(encoded_allocation_of_input: u32): u32;
-declare function hc_update_agent(encoded_allocation_of_input: u32): u32;
-declare function hc_remove_entry(encoded_allocation_of_input: u32): u32;
-declare function hc_get_entry(encoded_allocation_of_input: u32): u32;
-declare function hc_link_entries(encoded_allocation_of_input: u32): u32;
-declare function hc_query(encoded_allocation_of_input: u32): u32;
-declare function hc_send(encoded_allocation_of_input: u32): u32;
-declare function hc_start_bundle(encoded_allocation_of_input: u32): u32;
-declare function hc_close_bundle(encoded_allocation_of_input: u32): u32;
+declare namespace env {
+  function hc_debug(encoded_allocation_of_input: u32): u32;
+  function hc_call(encoded_allocation_of_input: u32): u32;
+  function hc_sign(encoded_allocation_of_input: u32): u32;
+  function hc_verify_signature(encoded_allocation_of_input: u32): u32;
+  function hc_commit_entry(encoded_allocation_of_input: u32): u32;
+  function hc_update_entry(encoded_allocation_of_input: u32): u32;
+  function hc_update_agent(encoded_allocation_of_input: u32): u32;
+  function hc_remove_entry(encoded_allocation_of_input: u32): u32;
+  function hc_get_entry(encoded_allocation_of_input: u32): u32;
+  function hc_link_entries(encoded_allocation_of_input: u32): u32;
+  function hc_query(encoded_allocation_of_input: u32): u32;
+  function hc_send(encoded_allocation_of_input: u32): u32;
+  function hc_start_bundle(encoded_allocation_of_input: u32): u32;
+  function hc_close_bundle(encoded_allocation_of_input: u32): u32;
+}
 
 export function debug(message: string): void {
   let encoded_allocation: u32 = serialize(message);
   let ptr = u32_high_bits(encoded_allocation);
-  hc_debug(encoded_allocation);
+  env.hc_debug(encoded_allocation);
   // local memory deals in unencoded pointers, unlike holochain
   free(ptr);
 }
