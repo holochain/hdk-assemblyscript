1e3102bbad3bab29a42fde843b5ca41131c8e224
Connor Turland <connorturland@gmail.com>
2018-09-10 16:22:14 -0400

    working memory utils

--- index.ts
+++ index.ts
@@ -1,31 +1,40 @@
 import "allocator/tlsf";
 
-declare function hc_debug(encoded_allocation_of_input: i32): i32;
-declare function hc_call(encoded_allocation_of_input: i32): i32;
-declare function hc_sign(encoded_allocation_of_input: i32): i32;
-declare function hc_verify_signature(encoded_allocation_of_input: i32): i32;
-declare function hc_commit_entry(encoded_allocation_of_input: i32): i32;
-declare function hc_update_entry(encoded_allocation_of_input: i32): i32;
-declare function hc_update_agent(encoded_allocation_of_input: i32): i32;
-declare function hc_remove_entry(encoded_allocation_of_input: i32): i32;
-declare function hc_get_entry(encoded_allocation_of_input: i32): i32;
-declare function hc_link_entries(encoded_allocation_of_input: i32): i32;
-declare function hc_query(encoded_allocation_of_input: i32): i32;
-declare function hc_send(encoded_allocation_of_input: i32): i32;
-declare function hc_start_bundle(encoded_allocation_of_input: i32): i32;
-declare function hc_close_bundle(encoded_allocation_of_input: i32): i32;
+import {
+  u32_high_bits,
+  u32_low_bits,
+  serialize,
+  deserialize,
+  free
+} from './utils';
 
-export function debug(message: String): void {
-  // 2 bytes per each char?
-  let byteLength: i32 = message.length*2;
-  // TODO: safeguard against overflow
-  let ptr: i32 = memory.allocate(byteLength);
-  for (let i: i32 = 0, strLen: i32 = message.length; i<strLen; i++) {
-    // use built-in store function to write to memory
-    // technically fits in an i8, but typescript function signature says otherwise
-    // TODO: store each value at the correct ptr offset
-    store<i32>(ptr, message.charCodeAt(i));
-  }
-  hc_debug(ptr);
-  memory.free(ptr);
+export {
+  u32_high_bits,
+  u32_low_bits,
+  serialize,
+  deserialize,
+  free
+}
+
+declare function hc_debug(encoded_allocation_of_input: u32): u32;
+declare function hc_call(encoded_allocation_of_input: u32): u32;
+declare function hc_sign(encoded_allocation_of_input: u32): u32;
+declare function hc_verify_signature(encoded_allocation_of_input: u32): u32;
+declare function hc_commit_entry(encoded_allocation_of_input: u32): u32;
+declare function hc_update_entry(encoded_allocation_of_input: u32): u32;
+declare function hc_update_agent(encoded_allocation_of_input: u32): u32;
+declare function hc_remove_entry(encoded_allocation_of_input: u32): u32;
+declare function hc_get_entry(encoded_allocation_of_input: u32): u32;
+declare function hc_link_entries(encoded_allocation_of_input: u32): u32;
+declare function hc_query(encoded_allocation_of_input: u32): u32;
+declare function hc_send(encoded_allocation_of_input: u32): u32;
+declare function hc_start_bundle(encoded_allocation_of_input: u32): u32;
+declare function hc_close_bundle(encoded_allocation_of_input: u32): u32;
+
+export function debug(message: string): void {
+  let encoded_allocation: u32 = serialize(message);
+  let ptr = u32_high_bits(encoded_allocation);
+  hc_debug(encoded_allocation);
+  // local memory deals in unencoded pointers, unlike holochain
+  free(ptr);
 }
---
+++ utils.ts
@@ -0,0 +1,71 @@
+import "allocator/tlsf";
+
+export function u32_high_bits(encoded_allocation: u32): u16 {
+  // right shift and explicit type cast it
+  let high: u16 = (encoded_allocation >> 16) as u16;
+  return high;
+}
+
+export function u32_low_bits(encoded_allocation: u32): u16 {
+  // type cast and remainder
+  let low: u16 = encoded_allocation as u16 % u16.MAX_VALUE;
+  return low;
+}
+
+// offset, length
+export function u32_merge_bits(high: u16, low: u16): u32 {
+  // left shift, bitwise or
+  return high as u32 << 16 | (low as u32);
+}
+
+export function check_encoded_allocation(encoded_allocation: u32): u32 {
+  
+  let offset = u32_high_bits(encoded_allocation);
+  let length = u32_low_bits(encoded_allocation);
+
+  if (length == 0) {
+    // error
+    return 1 << 16;
+  }
+
+  // switch to u32 from u16
+  let u32offset: u32 = offset;
+  let u32length: u32 = length;
+  let max: u32 = u16.MAX_VALUE;
+  if ((u32offset + u32length) > max) {
+    // ErrorPageOverflow
+    return 3 << 16;
+  }
+
+  // 0 is success
+  return 0;
+}
+
+// writes string to memory, then returns encoded allocation ref
+export function serialize(val: string): u32 {
+  let dataLength = val.length;
+  // each char takes two bytes, encoded
+  let ptr = memory.allocate(dataLength << 1);
+  //checkMem();
+  for (let i = 0; i < dataLength; ++i) {
+    store<u8>(ptr + i, val.charCodeAt(i));
+  }
+  let encoded_allocation = u32_merge_bits(ptr as u16, dataLength as u16);
+  return encoded_allocation;
+}
+
+export function deserialize(encoded_allocation: u32): string {
+  let offset = u32_high_bits(encoded_allocation);
+  let length = u32_low_bits(encoded_allocation);
+  let res: string = "";
+  for (let i: u16 = 0; i < length; i++) {
+    let charCode = load<u8>(offset + i);
+    res += String.fromCharCode(charCode);
+  }
+  memory.free(offset);
+  return res;
+}
+
+export function free(ptr: u32): void {
+  memory.free(ptr);
+}
