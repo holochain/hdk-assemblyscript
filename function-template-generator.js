39d11e498c977c63d57d6d6b19296cd197a5d517
Willem Olding <willemolding@gmail.com>
2018-09-28 10:18:14 +1000

    adds tests for function template generator

--- function-template-generator.js
+++ function-template-generator.js
@@ -8,34 +8,33 @@
  * @param      {Array<Param>} funcDef.parameters The list of parameters for the function as {name: string, type: string}
  * @param      {string} funcDef.returnType The return type as a string
  */	
-export function zomeFunctionString(funcDef) {
-	const paramsAsProperties = funcDef.parameters.reduce((acc, param) => acc + `${param.name};
+exports.makeFunctionString = function(funcDef) {
+	const paramsAsProperties = funcDef.parameters.reduce((acc, param) => acc + `${param.name}: ${param.type};
 	`, "")
-	const functionArgs = funcDef.parameters.reduce((acc, param) => acc + `${funcDef.Name}_Params.${param.name}`;
+	const functionArgs = funcDef.parameters.reduce((acc, param) => acc + `${funcDef.name}_Params.${param.name}`, "");
 
 
 	const codeString = `
-
-
-	class ${funcDef.Name}_Params {
+	class ${funcDef.name}_Params {
 		${paramsAsProperties}
 	}
 
-	class ${funcDef.Name}_ParamsHandler extends Handler {
-		// populate with the correcet callbacks
+	class ${funcDef.name}_ParamsHandler extends Handler {
+		// populate with the correct callbacks
 	}
 
-	export function ${funcdef.Name}(e: u32): u32 {
+	export function ${funcDef.name}(e: u32): u32 {
 		const jsonString = deserialize(e);
 
 		// parse the json in to the params object
-		const params = new ${funcDef.Name}_Params();
-		const handler = new ${funcDef.Name}_ParamsHandler(params);
-		parseString<${funcDef.Name}_ParamsHandler>(jsonString, handler)
+		const params = new ${funcDef.name}_Params();
+		const handler = new ${funcDef.name}_ParamsHandler(params);
+		parseString<${funcDef.name}_ParamsHandler>(jsonString, handler)
 
 		const resultString = _${funcDef.name}(${functionArgs});
 
 		return serialize(resultString);
 	}
 	`
+	return codeString
 }
---
+++ tests/test_template_generator.js
@@ -0,0 +1,28 @@
+const test = require('tape');
+const {makeFunctionString} = require('../function-template-generator');
+// import { makeFunctionString } from '../function-template-generator';
+
+test('Can generate a template for function with no parameters', function (t) {
+	const funcDef = {
+		name: 'testFunc',
+		parameters: [],
+		returnType: 'string'
+	}
+	const fString = makeFunctionString(funcDef);
+	t.comment(fString)
+
+	t.end();
+});
+
+
+test('Can generate a template for function with a single parameters', function (t) {
+	const funcDef = {
+		name: 'testFunc',
+		parameters: [{name: 'p1', type: 'string'}],
+		returnType: 'string'
+	}
+	const fString = makeFunctionString(funcDef);
+	t.comment(fString)
+
+	t.end();
+});
