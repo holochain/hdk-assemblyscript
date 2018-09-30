996014c728b6f6a20e923659e182595175d530c4
Willem Olding <willemolding@gmail.com>
2018-09-30 13:48:54 +1000

    tidy transform code

--- zome-functions-transform.js
+++ zome-functions-transform.js
@@ -30,7 +30,6 @@ exports.afterParse = function(parser) {
     ) {
 
       // unpack what we need from this function
-
       const func = {
         name: stmt.signature.parent.name.text,
         params: stmt.signature.parameters.map((param) => {
@@ -38,9 +37,6 @@ exports.afterParse = function(parser) {
         }),
         returnType: stmt.signature.returnType.name.text
       }
-
-      console.log(func)
-
 
       // rename the old function to be prefixed with an underscore
       stmt.signature.parent.name.text = "_"+func.name;
@@ -57,39 +53,9 @@ exports.afterParse = function(parser) {
       // add the new function to the AST as an exported function
       entrySrc.statements.push(callWrapperStmt);
 
-      // (optionally) add some data to the manifest.json
+      // TODO: add some data to the manifest.json
 
-
-
-      // to modify
-      /*
-      Create a string, then run "parseFile" on the string to get an AST of its own
-      const callWrapperStatement = parseFile(
-            `storageLoadWrapper()`, entrySrc.range.source.normalizedPath, true, null).program.sources[0].statements[0]
-      Then assign any results into the old AST, like
-      callWrapperStatement.expression.arguments.push(keyExpr)
-      */
     }
 
   })
-/*
-export function test_commit(val: string): string {
-  return commit_entry("message", val);
-}
-
-- search for functions with decorators
-- use the same function name
-- replace the function signature with consistent one
-- move the function signature var declaration into the main function body
-- take the return value and wrap it in serialize
-
-export function test_commit(e: u32): u32 {
-  let val: string = deserialize(e);
-  return serialize(commit_entry("message", val));
-}
-
-*/
-
-  // check out for examples of working with the ast
-  // https://github.com/etherts/ewasm-as/blob/master/assembly/src/transform.js
 }
