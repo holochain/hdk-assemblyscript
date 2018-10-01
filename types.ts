51d8bec52d683ad98483e81e67b1d0fc564ab2b0
Willem Olding <willemolding@gmail.com>
2018-10-01 19:30:11 +1000

    adds types

---
+++ types.ts
@@ -0,0 +1,41 @@
+@unmanaged
+export class CommitResult {
+	hash: string;
+}
+
+@unmanaged
+export class Header {
+	entryType: string;
+	timestamp: string;
+	link: string;
+	entryHash: string;
+	entrySignature: string;
+	linkSameType: boolean;
+}
+
+@unmanaged
+export class Entry {
+	content: string;
+	entryType: string;
+}
+
+@unmanaged
+export class GetResult {
+	header: Header;
+	entry: Entry;
+
+	constructor() {
+		this.header = new Header();
+		this.entry = new Entry();		
+	}
+}
+
+@unmanaged
+export class InitGlobalsOutput {
+    app_name: string
+    app_dna_hash: string
+    app_agent_id_str: string
+    app_agent_key_hash: string
+    app_agent_initial_hash: string
+    app_agent_latest_hash: string
+}
