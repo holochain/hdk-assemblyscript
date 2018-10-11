90bbee43c0ac89e3e4bd427781ebab12c9db76d9
Willem Olding <willemolding@gmail.com>
2018-10-11 14:29:15 +1100

    adds our holochain/assemblyscript as a node requirement and puts back NodeKind enum

--- transforms/index.js
+++ transforms/index.js
@@ -24,7 +24,7 @@ exports.afterParse = function(parser) {
   
   entrySrc.statements.forEach(stmt => {
     if (
-      stmt.kind === 49 && //NodeKind.FUNCTIONDECLARATION && 
+      stmt.kind == NodeKind.FUNCTIONDECLARATION && 
       stmt.decorators &&
       stmt.decorators.length &&
       stmt.decorators.some(d => d.name.text === "zome_function")
