6dd192361f65a4b65d6e69a6153d44501051966a
Michael Dougherty <maackle.d@gmail.com>
2018-10-11 20:48:19 -0400

    hctest helper injects a per-test app instance

--- examples/appSpec/test/index.js
+++ examples/appSpec/test/index.js
@@ -1,18 +1,10 @@
-// This test file uses the tape testing framework. 
+// This test file uses the tape testing framework.
 // To learn more, go here: https://github.com/substack/tape
-const test = require('tape');
 
-function loadApp() {
-    // instantiate an app from the Genome JSON bundle
-    const app = Container.loadAndInstantiate("dist/bundle.json")
-    // activate the new instance
-    app.start()
-    return app
-}
+import {hctest} from './test-helper'
 
-test('test commit_entry', (t) => {
+hctest('test commit_entry', (app, t) => {
   t.plan(1)
-  const app = loadApp()
   let result = app.call("three", "main", "test_commit_entry", "hello")
   let obj
   try {
@@ -23,18 +15,16 @@ test('test commit_entry', (t) => {
   t.deepEqual(obj, { address: 'QmZsRsCTUGBy7ox5hiitArDfCjxUJxvoLLZBwZrmCy5wv4' })
 })
 
-test('test get_entry', (t) => {
+hctest('test get_entry', (app, t) => {
   t.plan(1)
-  const app = loadApp()
   app.call("three", "main", "test_commit_entry", "hello")
   let result = app.call("three", "main", "test_get_entry", "QmZsRsCTUGBy7ox5hiitArDfCjxUJxvoLLZBwZrmCy5wv4")
   // TODO: fix this once GET gets fixed
   t.equal(result, '"message"')
 })
 
-test('test init_globals', (t) => {
+hctest('test init_globals', (app, t) => {
   t.plan(1)
-  const app = loadApp()
   let result = app.call("three", "main", "test_init_globals", "")
   let obj
   try {
